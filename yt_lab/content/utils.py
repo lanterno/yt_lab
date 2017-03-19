c3b4c37e3156ef9ec351b7c20849fe29ab62df9c
zee93 <ahmeds.elghareeb@gmail.com>
2017-03-19 23:16:09 +0200

    Implemented update_source_info func using google api client

--- yt_lab/content/utils.py
+++ yt_lab/content/utils.py
@@ -1,6 +1,30 @@
+from apiclient.discovery import build
+import os
+
+yt = build('youtube', 'v3', developerKey=os.environ.get("GOOGLE_API_KEY"))
+
+
 def update_source_info(source):
-    print("Updating Source info..")
+    '''
+    Input: a Source object
+    function: get title from youtube API
+    '''
+    # get name or title
+    if source.typ == source.CHANNEL:
+        response = yt.channels().list(part="snippet", fields="items(snippet(title))", id=source.youtube_id).execute()
+        source.title = response['items'][0]['snippet']['title']
+    elif source.typ == source.PLAYLIST:
+        response = yt.playlists().list(part="snippet", fields="items(snippet(title))", id=source.youtube_id).execute()
+        source.title = response['items'][0]['snippet']['title']
+    source.save()
 
 
 def update_source_videos(source):
-    print("Updating Source videos..")
+    '''
+    input: a Source object
+    function: get_or_update all related objects to the source provided
+    '''
+    if source.typ == source.PLAYLIST:
+        pass
+    elif source.typ == source.CHANNEL:
+        pass
