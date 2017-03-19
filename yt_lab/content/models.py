bd3d9d1d1c13fcb1a0d0351b82891fdf5a91a43e
zee93 <ahmeds.elghareeb@gmail.com>
2017-03-19 22:04:53 +0200

    Validating source URL and extracted source id.

--- yt_lab/content/models.py
+++ yt_lab/content/models.py
@@ -6,12 +6,15 @@ class Source(models.Model):
     This is a source of videos. It can be anything with a url that links to a webpage that has videos.
     Currently the system can handle only two types on Content sources; Playlists and Channels
     '''
+    PLAYLIST = 0
+    CHANNEL = 1
     SOURCE_TYPES = (
-        (0, 'Playlist'),
-        (1, 'Channel'),
+        (PLAYLIST, 'Playlist'),
+        (CHANNEL, 'Channel'),
     )
     url = models.URLField()
     title = models.CharField(max_length=400, null=True, blank=True)
+    youtube_id = models.CharField(max_length=150)
     typ = models.PositiveSmallIntegerField(choices=SOURCE_TYPES, null=True, blank=True)
 
     class Meta:
--- yt_lab/content/serializers.py
+++ yt_lab/content/serializers.py
@@ -1,4 +1,6 @@
+import re
 from rest_framework import serializers
+
 
 from .models import Source, Video
 
@@ -7,7 +9,30 @@ class SourceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Source
-        fields = ('id', 'url', 'title', 'typ')
+        fields = ('id', 'url', 'title', 'typ', 'youtube_id')
+        read_only_fields = ('id', 'title', 'youtube_id', 'typ')
+
+    def validate(self, data):
+        '''
+        - Apply all default validations
+        - make sure provided URL is valid
+        - parse the youtube_id and type
+        return data or raise validation error
+        '''
+        data = super().validate(data)
+        channel_pattern = re.compile(r'.*youtube.com/(channel|user)/(?P<id>[ A-Za-z0-9_-]*)')
+        playlist_pattern = re.compile(r'.*list=(?P<id>[ A-Za-z0-9_-]*)')
+
+        if channel_pattern.match(data['url']):
+            data['youtube_id'] = channel_pattern.match(data['url']).groupdict()['id']
+            data['typ'] = Source.CHANNEL
+        elif playlist_pattern.match(data['url']):
+            data['youtube_id'] = playlist_pattern.match(data['url']).groupdict()['id']
+            data['typ'] = Source.PLAYLIST
+        else:
+            raise serializers.ValidationError("Source URL couldn't be validated")
+        print('data')
+        return data
 
 
 class VideoSerializer(serializers.ModelSerializer):
